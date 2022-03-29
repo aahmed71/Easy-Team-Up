ee2ead69e1a902dfc8f3787c4b1635deede5360d
chiuclau <chiuclau@usc.edu>
2022-03-28 19:28:24 -0700

    created user portal

---
+++ EasyTeamUp/app/src/main/java/com/example/easy_team_up/UserPortal.java
@@ -0,0 +1,15 @@
+package com.example.easy_team_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPortal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_portal);
+        
+    }
+}
