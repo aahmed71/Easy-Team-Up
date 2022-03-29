459f7ff06c95f4942a0e3a924730b2beba6f6892
chiuclau <chiuclau@usc.edu>
2022-03-29 11:55:11 -0700

    filled edit form programatically

--- EasyTeamUp/app/src/main/java/com/example/easy_team_up/EditEvent.java
+++ EasyTeamUp/app/src/main/java/com/example/easy_team_up/EditEvent.java
@@ -2,10 +2,13 @@ package com.example.easy_team_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -18,6 +21,35 @@ public class EditEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_event);
+        DBHelper DB;
+
+        //get intent
+        Intent intent = getIntent();
+        Integer eventId = intent.getIntExtra("eventId", -1);
+        if(eventId == -1){
+            System.out.println("event does not exist");
+            return;
+        }
+        else System.out.println("editing event: " + eventId);
+
+        //grab event details and flesh out the hints
+        DB = new DBHelper(this);
+        Cursor eventInfo = DB.getEventById(eventId);
+        eventInfo.moveToFirst();
+
+        String eventName = eventInfo.getString(2);
+        System.out.println("event name: " + eventInfo.getString(2));
+        EditText editName = (EditText) findViewById(R.id.eventName);
+        editName.setHint(eventName);
+
+        String eventType = eventInfo.getString(3);
+        EditText editType = (EditText) findViewById(R.id.eventType);
+        if(eventType != null) editType.setHint(eventType);
+
+        String eventLocation = eventInfo.getString(12);
+        System.out.println("event location: " + eventLocation);
+        EditText editLocation = (EditText) findViewById(R.id.location);
+        if(eventLocation != null) editLocation.setHint(eventLocation);
 
         Spinner monthSpinner = findViewById(R.id.month);
         ArrayList<String> arrayList = new ArrayList<>();
