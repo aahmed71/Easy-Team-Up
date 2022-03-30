3a2d0afdc60f40898b741b644b28782fdeac484c
aahmed71 <aaron.ahmed71@gmail.com>
2022-03-29 17:25:14 -0700

    Updated ViewEvent

--- EasyTeamUp/app/src/main/java/com/example/easy_team_up/ViewEvent.java
+++ EasyTeamUp/app/src/main/java/com/example/easy_team_up/ViewEvent.java
@@ -30,19 +30,42 @@ public class ViewEvent extends AppCompatActivity {
         Cursor res = DB.getEventById(eventId);
         System.out.println("result size: " + res.getCount());
         res.moveToFirst();
-        String location = res.getString(12);
-        Cursor userRes = DB.getNameFromUserId(res.getInt(2));
+        String location = res.getString(14);
+        Cursor userRes = DB.getNameFromUserId(Integer.parseInt(res.getString(1)));
         userRes.moveToFirst();
         //setting title
-        TextView titleView = findViewById(R.id.eventTitle);
+        TextView titleView = findViewById(R.id.eventTitle2);
         titleView.setText(res.getString(2));
         //setting host
         TextView hostView = findViewById(R.id.host);
         //userRes.getString(1)
-        hostView.setText("a host");
+        String username = userRes.getString(1);
+        hostView.setText(username);
         //setting location
         TextView locationView = findViewById(R.id.location);
         locationView.setText(location);
+
+        TextView dateView = findViewById(R.id.date);
+        String date = res.getString(6) + ", " + res.getInt(7) + " " + res.getInt(8);
+        dateView.setText(date);
+
+        TextView startView = findViewById(R.id.startTime);
+        startView.setText("" + res.getInt(4));
+
+        TextView endView = findViewById(R.id.endTime);
+        endView.setText("" + res.getInt(5));
+
+        TextView dueDateView = findViewById(R.id.dueDate);
+        String dueDate = res.getString(9) + ", " + res.getInt(10) + " " + res.getInt(11);
+        dueDateView.setText(dueDate);
+
+        TextView dueTimeView = findViewById(R.id.dueTime);
+        String dueTime = "" + res.getInt(12);
+        dueTimeView.setText(dueTime);
+
+        TextView typeView = findViewById(R.id.type);
+        typeView.setText(res.getString(3));
+
         button = findViewById(R.id.goBack);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
