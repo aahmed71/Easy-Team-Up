5cd82b28c06b468d6969454f26aee90f0efd8ed0
chiuclau <chiuclau@usc.edu>
2022-04-12 11:23:34 -0700

    Created page for notifications

---
+++ EasyTeamUp/app/src/main/java/com/example/easy_team_up/Notification.java
@@ -0,0 +1,13 @@
+package com.example.easy_team_up;
+public class Notification {
+    Integer id;
+    Integer userId;
+    String description;
+
+    public Notification(Integer id, Integer userId, String description){
+        this.id = id;
+        this.userId = userId;
+        this.description = description;
+    }
+}
+
